78c6a480e9b750dda9482dff916e6004ef03bc0e
John Donnellan <donnel2@cooper.edu>
2020-09-18 21:07:36 -0500

    Starting HW 2 folder, not assigned yet but getting RK4 implementation out of the way.

---
+++ Homework/Homework_2/python/run_simulation.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import integrators as intg
+
+
+
+def f(t, x, u):
+    return np.array([x[1], -x[0]+u])
+
+
+t = 0; x = np.array([0, 1]); u = 0
+dt = 0.1; n = 100
+
+integrator = intg.Heun(dt, f)
+
+
+t_history = [0]
+x_history = [x]
+for i in range(n):
+    
+    x = integrator.step(t, x, u)
+    t = (i+1) * dt
+
+    t_history.append(t)
+    x_history.append(x)
+
+intg.__doc__
+plt.figure()
+plt.plot(t_history, x_history)
+plt.show()
