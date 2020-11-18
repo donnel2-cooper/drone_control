f359b2be0f0eda2a9f2e00773e7b503ebe346caa
harry <harry98530@gmail.com>
2020-11-18 01:11:08 -0500

    Fixed beam cascade PID, updated beam parameters

---
+++ Lectures/Motor_Workshop/beam_cascade_PID.py
@@ -0,0 +1,92 @@
+import numpy as np
+import beam_parameters as P
+from integrators import get_integrator
+# import integrators as intg
+from pid import PIDControl
+import matplotlib.pyplot as plt
+
+class Controller:
+    def __init__(self, kp, ki, kd, limit, sigma, Ts, flag=True):
+        self.kp = kp # Proportional control gain
+        self.ki = ki # Integral control gain
+        self.kd = kd # Derivative control gain
+        self.limit = limit # The output saturates at this limit
+        self.sigma = sigma # dirty derivative bandwidth is 1/sigma 
+        self.beta = (2.0*sigma-Ts)/(2.0*sigma+Ts)
+        self.Ts = Ts # sample rate 
+        self.flag = flag
+        self.PID_object = PIDControl(self.kp, self.ki, self.kd, self.limit, self.sigma, self.Ts, self.flag)        
+
+    def update(self, r, y):
+        return self.PID_object.PID(r,y)
+
+class System:
+    def __init__(self, J, d, b, omega, Ts):
+        self.J = J # Moment of Inertia
+        self.d = d # Distance between props and center of rotation
+        self.b = b # Damping coefficient
+        self.omega = omega # initial state
+        self.Ts = Ts
+        self.intg_omega = get_integrator(Ts,self.omega_dot)
+        self.intg_theta = get_integrator(Ts,self.theta_dot)
+        
+    def omega_dot(self, t,omega,u):
+        self.omega_dot = self.d/self.J*u - self.b/self.J*omega #Psuedo code EOM
+        return self.omega_dot 
+
+    def theta_dot(self,t,theta,u):
+        return u
+
+    def update_omega(self,t,y,u):
+        return self.intg_omega.step(t,y,u)
+    
+    def update_theta(self,t,y,u):
+        return self.intg_theta.step(t,y,u)
+        
+    
+# Init system and feedback controller
+system = System(P.J,P.d,P.b,P.x0, P.Ts)
+PIcontroller = Controller(P.kp,P.ki,0,100000,P.sigma,P.Ts,flag=True)
+Dcontroller = Controller(P.kd,0,0, P.umax, P.sigma, P.Ts,flag = True)
+# Simulate step response
+t_history = [0]
+omega_history = [0]
+theta_history = [0]
+upi_history = [0]
+ud_history = [0]
+
+r = 0.78 # 0.78 rad = 45 deg
+theta = 0
+omega = 0
+t = 0
+
+for i in range(P.nsteps):
+    upi = PIcontroller.update(r,theta)
+    #for j in range(1,10):
+    for j in range(1,2):
+        ud = Dcontroller.update(upi, omega)
+        omega = system.update_omega(t,omega,upi-ud)
+    t += P.Ts
+    theta = system.update_theta(t,theta,omega)
+    t_history.append(t)
+    omega_history.append(omega)
+    theta_history.append(theta)
+    upi_history.append(upi)
+    ud_history.append(ud)
+
+# Plot response theta due to step change in r
+plt.figure(figsize=(8,6))
+plt.plot(t_history,omega_history, label="$\omega$")
+plt.plot(t_history,theta_history, label="$\Theta$")
+plt.legend()
+plt.show()
+
+print(f'Final angle = {theta_history[-1]}')
+
+# Plot actuation signal
+plt.figure(figsize=(8,6))
+plt.plot(t_history,np.add(upi_history, ud_history), label="U total")
+plt.legend()
+plt.xlabel("Time [s]")
+plt.ylabel("Actuation [V]")
+plt.show()
--- Lectures/Motor_Workshop/beam_parameters.py
+++ Lectures/Motor_Workshop/beam_parameters.py
@@ -7,10 +7,10 @@ umax = 10/1000 # g
 # Simulation
 x0 = 0 # rad/s
 Ts = 5e-3 # s
-nsteps = 1000
+nsteps = 2000
 
 # PID controller
-kp = 0.1
-ki = 10
-kd = 0
+kp = 0.01
+ki = 0.005
+kd = 0.001
 sigma = 0.1
